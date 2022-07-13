6291a795beea2d632efcfb332fbe6dae6789755d
danyrose <danyrose1995@gmail.com>
2022-07-13 16:30:23 +0200

    wip calcul zone

---
+++ apis/distanceAPI.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export async function getDistanceByRoad(start, end) {
+  const url = "https://api.openrouteservice.org/v2/directions/driving-car/json";
+  const body = {
+    coordinates: [
+      [start[0], start[1]],
+      [end[0], end[1]],
+    ],
+    units: "km",
+  };
+  const config = {
+    headers: {
+      Authorization: process.env.ORS_API_KEY,
+    },
+  };
+  const res = await axios.post(url, body, config);
+  const data = res.data;
+  const roadDistance = data.routes[0].summary.distance;
+  console.log(roadDistance);
+  return roadDistance;
+}
