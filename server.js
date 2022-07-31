7e31b07e940fd860045d8988b2b6de5e9b3b8187
dany rose <danyrose1995@gmail.com>
2022-07-31 22:29:54 +0200

    https

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+import fs from "fs";
+import http from "http";
+import https from "https";
 import express from "express";
 import dotenv from "dotenv";
 import apiRouter from "./routers/apiRouter.js";
@@ -5,24 +8,36 @@ import cors from "cors";
 import path from "path";
 dotenv.config();
 
-const server = express();
-server.use(cors());
+const app = express();
+app.use(cors());
 
 // API
-server.use(express.json());
-server.use("/api", apiRouter);
+app.use(express.json());
+app.use("/api", apiRouter);
 
 // client react
 const buildPath = path.resolve() + "/public/build";
-server.use(express.static(buildPath));
-server.get("/*", (req, res) => {
+app.use(express.static(buildPath));
+app.get("/*", (req, res) => {
   res.sendFile(buildPath + "/index.html", (error) => {
     console.log(error);
     res.status(404).end();
   });
 });
 
+// Server
+const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 8080;
+
+//  const server = http.createServer(app);
+const server = https.createServer(
+  {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert"),
+  },
+  app
+);
+
 server.listen(PORT, () => {
-  console.log("server listen on http://localhost:" + PORT);
+  console.log(`server listen on ${HOST}:${PORT}`);
 });
